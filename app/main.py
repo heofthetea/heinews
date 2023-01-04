0b150db6ad3f3cc9aa2bf1820f750e57665761bc
ERROR Emil Not Found <fyoug8gle@gmail.com>
2023-01-04 15:37:16 +0100

    copied static folder

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-"""
+
 # uncomment this to run on your own machine 
 from os import getcwd, chdir
 print(getcwd())
 
 #set this path to the directory `main.py` is in
 chdir("C:\\Coding Stuff\\GAP\\hhg-news\\heiNEws\\app")
-"""
+
 from app import create_app
 # specifies application host as (domain, port)
 __HOST__ = ("127.0.0.1", 5000)
